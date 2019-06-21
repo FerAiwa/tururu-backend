a5911c39e3b46ad7ebd63efd960550bf89576ca4
Fer Ayguavives <fer.aiwa@gmail.com>
2019-06-21 10:55:05 +0200

    Testing search query remote

--- app/webserver/interfaces/controllers/user/search-users-ct.js
+++ app/webserver/interfaces/controllers/user/search-users-ct.js
@@ -4,6 +4,7 @@ import userUC from '../../../../domain/use-cases/user';
  */
 async function searchUsers(req, res, next) {
   const { q } = req.query;
+  console.log('searching user' , q);
 
   try {
     const usersPublicInfo = await userUC.searchUsersUC(q);
